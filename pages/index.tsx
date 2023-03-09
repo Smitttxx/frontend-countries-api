2f2f09f21158b9e653820f7eb7ed76be5b0d074f
Smitttxx <53533374+Smitttxx@users.noreply.github.com>
2023-03-09 16:55:44 +0000

    Feat: :tada: Begin Project- Next setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Container } from "semantic-ui-react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -101,6 +102,7 @@ export default function Home() {
               Discover and deploy boilerplate example Next.js&nbsp;projects.
             </p>
           </a>
+          <Container>TEST CONTAINER</Container>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -119,5 +121,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
